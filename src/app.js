2615e1beb8b4cf7e92383a6773b3661b298405d1
guilhiz <guilhermevilelasobral@gmail.com>
2023-01-18 11:20:48 -0300

    feat: add function to register the user in the database

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express, { json } from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import dayjs from "dayjs";
 import joi from "joi";
-
 
 const app = express();
 dotenv.config();
@@ -11,20 +11,29 @@ app.use(cors());
 app.use(json());
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-const PORT = 5000
+const PORT = 5000;
 let db;
 
 try {
-	await mongoClient.connect();
+  await mongoClient.connect();
 } catch (err) {
-	console.log(err);
+  console.log(err);
 }
 
 db = mongoClient.db();
-const registries = db.collection("registries")
-const users = db.collection("users")
+const registries = db.collection("registries");
+const users = db.collection("users");
 
+app.post("/register", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  try {
+    users.insertOne({ name, email, password, confirmPassword });
 
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 
 
